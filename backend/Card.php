a43751dde9fd9ebe89e507f3e575beddc7c30c64
David-Floy <pc.tischer@gmail.com>
2025-07-30 19:13:22 +0200

    Added Header

--- backend/Card.php
+++ backend/Card.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 /**
  * Card-Klasse für eine Dixit-Karte
  */
