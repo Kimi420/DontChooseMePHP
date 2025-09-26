5984a1bd13ff3e24852230c53bbb705fa89cca99
David-Floy <pc.tischer@gmail.com>
2025-09-26 14:56:59 +0200

    added Time to result phase debug

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { getGameState, giveHint, chooseCard, vote, nextRound, resetMatch } from './api';
 import './AppLayout.css';
 import audioManager from './AudioManager';
